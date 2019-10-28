e5e2b7d4a4850cf756cdab73684562ae346ab8a3
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-28 05:03:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -259,16 +259,25 @@ end
 
 def game_hash
   {
-    away: { team_name: 'Charlotte Hornets',
+    away: { 
+            team_name: 'Charlotte Hornets',
             colors: %w[Turquoise Purple],
             players: [
-              {  },
-              {  },
-              {  },
-              {  },
+              { player_name: 'Jeff Adrien',
+                number: 4,
+                shoe: 18,
+                points: 10,
+                rebounds: 1,
+                assists: 1,
+                steals: 2,
+                blocks: 7,
+                slam_dunks: 2 }, },
+              { },
+              { },
+              { },
               { }
             ] },
-    home: {  }
+    home: { }
   }
 end
 
